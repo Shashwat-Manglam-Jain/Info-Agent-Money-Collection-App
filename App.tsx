139cf378efa2beb0c05daef2676669db2d070c44
unknown <jayshankaryadav@cyberwala.in>
2026-02-13 17:19:08 +0530

    pull

--- App.tsx
+++ App.tsx
@@ -4,8 +4,22 @@ import { AppProvider } from './src/appState/AppProvider';
 import { ThemedStatusBar } from './src/components/ThemedStatusBar';
 import { ThemeProvider } from './src/theme';
 import { RootNavigator } from './src/navigation/RootNavigator';
+import { getDb } from './src/db/db';
 
 export default function App() {
+
+
+
+async function checkAgentsTable() {
+  const db = await getDb();
+
+  const result = await db.getAllAsync("PRAGMA table_info(agents);");
+
+  console.log("Agents Table Columns:", result);
+}
+
+checkAgentsTable();
+
   return (
     <SafeAreaProvider>
       <ThemeProvider>
